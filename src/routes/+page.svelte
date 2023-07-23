9af3c88e0ef60b67ccd701900e2f4cdf8fef25f6
atprana develop <aditya.tresnaprana@gmail.com>
2023-07-23 09:56:43 +0000

    Develop HTML Structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import TodoItem from "$lib/todo-item.svelte";
+    const title="Todos";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<h1>SvelteKit {title}</h1>
+
+<form action="">
+    <input type="text" name="todo" aria-label="Add a todo" placeholder="+ type to add todo">
+</form>
+
+<TodoItem />
+<TodoItem />
+<TodoItem />
